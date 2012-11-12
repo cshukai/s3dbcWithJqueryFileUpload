e657ca8e044aed44119ce1a4606d07273561c581
Shu-Kai Chang <cshukai@gmail.com>
2012-11-12 16:57:08 -0600

    namespacing

--- js/customUI.js
+++ js/customUI.js
@@ -1,74 +1,52 @@
-//setting up  credential
+//setting up global variables
+window.kinomics={};
+kinomics.s3dbURL={};
+kinomics.s3db_usrname={};
+kinomics.s3db_password={};
+kinomics.s3db_collectionId={};
+kinomics.s3db_ruleId={};
+var s3db_key={};
+
+
+var collection_id='89';
+var rule_id='99';
 var s3dbURL='http://204.232.200.16/uabs3db';
 var username='shukai';
 var password='12345';
-var s3db_key={};
-var collection_id='89';
-var rule_id='99';
-
-
-var generateJFileUploadUIs=function(containerID){
-
-    var buttonText=document.createElement('span');
-    buttonText.textContent='select file...';
- 
-    var addFileButton=document.createElement('input');
-    addFileButton.id='importer';
-    addFileButton.type='file';
-    addFileButton.name='files[]';
-    $('#importer').attr('multiple','');
-    var addFileSpan=document.createElement('span');
-    addFileSpan.setAttribute('class','btn btn-success fileinput-button');
-    addFileSpan.appendChild(buttonText);
-    addFileSpan.appendChild(addFileButton);
-    
 
 
 
-    var uploadFileBtnText=document.createElement('span');
-    uploadFileBtnText.textContent='upload to S3DB';
-    var uploadFileBtn=document.createElement('button');
-    uploadFileBtn.type='submit';
-    uploadFileBtn.setAttribute('class','btn btn-primary start');
-    uploadFileBtn.appendChild(uploadFileBtnText);
 
-    
-
-    var ButtonBarDiv=document.createElement('div');
-    ButtonBarDiv.setAttribute('class','row fileupload-buttonbar');
-    ButtonBarDiv.appendChild(addFileSpan);
-    ButtonBarDiv.appendChild(uploadFileBtn);
-
-    var fileLoadingDiv=document.createElement('div');
-    fileLoadingDiv.setAttribute('class','fileupload-loading');
-    fileLoadingDiv.style.visibility='hide';
-
-    
-    var presentTable=document.createElement('table');
-    presentTable.setAttribute('role','presentation');
-    presentTable.setAttribute('class','table table-striped');
-    presentTable.style.visibility='hide';
-    
-    var fileUploadForm=document.createElement('form');
-    fileUploadForm.id='fileupload';
-    fileUploadForm.action='url:alterLater';
-    fileUploadForm.method='POST';
-    fileUploadForm.enctype="multipart/form-data";
-    fileUploadForm.appendChild(ButtonBarDiv);
-    fileUploadForm.appendChild(fileLoadingDiv);
-    fileUploadForm.appendChild(presentTable);
-
-
-    var container=document.getElementById(containerID);
-    container.appendChild(fileUploadForm);
+var updateS3dbStatment=function(s3dbURL,s3db_key,collection_id,rule_id){
+    s3dbc.setDeployment(s3dbURL);
+    s3dbc.setJSONP(false);
+    s3dbc.login(username, password, function (err, key) {
 };
 
+var deleteS3dbStatement=function(s3dbURL,statementId,callback){
+    s3dbc.setDeployment(s3dbURL);
+    s3dbc.setJSONP(false);
+    s3dbc.login(username, password, function (err, key) {
+    console.log(err);
+    console.log(key);
+    s3dbc.deleteStatement(statementId,callback);
+       console.log(err);
+       console.log(results);
+   });
+
+};
+
+
+
+///////////////////////////////////////////////////////for adding file to s3db////////////////////////////////////////////
 
 var send2S3dbFmForm=function(s3dbURL,s3db_key,collection_id,rule_id){
   // fileupload is the id of the form which jquery file upload applied to
  $("#fileupload").attr('action', s3dbURL+'/multiupload.php?key='+s3db_key+'&collection_id='+collection_id+'&rule_id='+rule_id+'&format=json');
 
 };
+
+
 
 var send2S3dbFmCustomUI=function(inputFileId,filesList,s3dbURL,s3db_key,collection_id,rule_id){
   // fileupload is the id of the form which jquery file upload applied to
@@ -84,6 +62,8 @@ var send2S3dbFmCustomUI=function(inputFileId,filesList,s3dbURL,s3db_key,collecti
 
 /*
 @purpose: wrapping up the process of application of JQuery file upload
+@dependencies: function send2S3dbFmCustomUI
+
 */
 var setupFileUploader=function(inputFileId,s3dbURL,collection_id,rule_id){
 
@@ -99,3 +79,4 @@ var setupFileUploader=function(inputFileId,s3dbURL,collection_id,rule_id){
    });
 };
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
