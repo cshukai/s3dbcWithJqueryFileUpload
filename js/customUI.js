cecfafafc6d4b95b7d589e1415f2790168be86cf
Shu-Kai Chang <cshukai@gmail.com>
2012-11-15 12:18:01 -0600

    addFileMeta

--- js/customUI.js
+++ js/customUI.js
@@ -33,6 +33,57 @@ var setupFileUploader=function(inputFileId,s3db_key,collection_id,rule_id){
    
 };
 
+
+
+var displayFileInfoByCollectionAndRule=function(collectionID,ruleID){
+      // display file name, file size and url of selected file
+     s3dbc.selectItemsByCollection(collectionID, function(err,results){
+          console.log(err);
+          console.log(results);
+         // var itemsId=[];
+          for(var i=0;i<results.length;i++){
+              //itemsId[i]=results[i].item_id;
+              kinomics.s3db_selected_items=results[i].item_id;
+          }
+          
+     });
+
+ };
+
+
+ var getFileMetaByRuleIdInTheCollection=function(ruleID,collectionID,callback){
+
+   s3dbc.selectItemsByCollection(collectionID, function(err,results){
+    //console.log(err);
+    //console.log(results);
+    var itemsId=[];
+    for(var i=0;i<results.length;i++){
+     (function(i){
+       s3dbc.selectStatementsByRuleAndItem(ruleID,results[i].item_id,function(err,results){
+        // console.log(err);
+         //console.log(results);
+         var tableRow=document.createElement('tr');
+         var td_fileSize=document.createElement('td');
+         var td_fileName=document.createElement('td');
+         var td_fileLink=document.createElement('td');
+
+         td_fileSize.textContent=results[0].file_size;
+         td_fileName.textContent=results[0].file_name;
+         td_fileLink.textContent=results[0].value.match(/"([^"]+)"/)[1];
+         tableRow.appendChild(td_fileSize);
+         tableRow.appendChild(td_fileName);
+         tableRow.appendChild(td_fileLink);
+
+          callback(tableRow);       
+       });
+
+     })(i);
+   }
+ });
+
+ };
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -48,8 +99,9 @@ kinomics.s3db_ruleId={};
 kinomics.s3db_key={};
 kinomics.s3dbc_deleteStatement={};
 kinomics.s3dbc_updateStatement={};
-
+kinomics.displayFileInfoByCollectionAndRule={};
 kinomics.setupFileUploader={};
+kinomics.fileInfo={};
 kinomics.login=function(username,password,s3dbURL,callback){
      // setting up s3dbc's env
      s3dbc.setDeployment(s3dbURL);
@@ -63,12 +115,12 @@ kinomics.login=function(username,password,s3dbURL,callback){
      kinomics.setupFileUploader=setupFileUploader;
      kinomics.s3dbc_deleteStatement=s3dbc.deleteStatement;
      kinomics.s3dbc_updateStatement= s3dbc.updateStatement;
-
+     kinomics.getFileMetaByRuleIdInTheCollection=getFileMetaByRuleIdInTheCollection; 
      callback;
    });
 };
 
-
+// get the listing  file function here ......
 var collection_id='89';
 var rule_id='99';
 var s3dbURL='http://204.232.200.16/uabs3db';
